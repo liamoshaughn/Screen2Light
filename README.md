cd02501cc15bacc7e03f41d9265642ecfe3985c2
Liam O'Shaughnessy <liam@jpmedia.com.au>
2023-08-08 22:07:48 +0930

    Seperated functions and added another light mode

--- README.md
+++ README.md
@@ -6,13 +6,9 @@ Continuously calculates the average colour of your screen and sets your LIFX bul
 
 Watch movies *(or anything you want on your screen)* in style. Like [THIS!](https://youtu.be/WHCtUvEJXq0)
 
-![image](https://user-images.githubusercontent.com/5826484/129788092-e4969cd2-8238-430c-a69d-73ffbd7870a3.png)
-
-
 (*seeks to improve over the original lifxscreen at https://github.com/frakman1/lifxscreen*)
 
-- This version uses the much faster LAN protocol (vs the slower HTTP API).
-- Splits the screen into left and right segments and allows you to control two sets of bulbs independently. 
+- This version uses the much faster LAN protocol 
 - Crops the screen so as not to take the black portion into account when calculating the average colour. 
 - Better black screen colour handling. 
 
@@ -21,19 +17,9 @@ Tested on a Windows-7, 64bit machine. Python version 2.7.5.
 
 ## Prerequisites:
 
-* [PIL](http://effbot.org/downloads) - Screen Grabber which relies on [VCForPython27](http://www.microsoft.com/en-us/download/details.aspx?id=44266)
-
 * colour - Colour Convertions and Manipulations  (https://pypi.python.org/pypi/colour/)
 
-* lazylights - The actual LIFX driver.  https://github.com/mpapi/lazylights/tree/2.0
-
-(Be sure to install the 2.0 branch of lazylights)
-```pip install git+https://github.com/mpapi/lazylights@2.0```
-
-
-* Around line 41 *(createBulb('10.10.10.2','XX:XX:XX:XX:XX:XX'))* , Replace both the IP address and **'XX:XX:XX:XX:XX:XX'** with the MAC addresses of your bulbs. You should be able to find the MAC Address on the bulb itself, or on your router page, or by using my iOS app [LIFX Ambience](https://apps.apple.com/au/app/lifx-ambience/id1012474625#?platform=iphone)
-
-![image](https://user-images.githubusercontent.com/5826484/129788952-ddad1de1-9379-4bfb-a2d7-b4e6a784d3ad.png)
+* bitstring-3.1.9
 
 
 ## Syntax:
--- lifxscreen2.py
+++ lifxscreen2.py
@@ -1,35 +1,172 @@
-#!/usr/bin/env python
-# coding=utf-8
 from time import sleep
 from PIL import ImageGrab
-from lifxlan import BLUE, GREEN, LifxLAN
-import time
-import os
+from lifxlan import LifxLAN
+from lifxlan.msgtypes import GetLabel, StateLabel
 from colour import Color
 import sys
 import math
 import binascii
 from tkinter import *
 import asyncio
+import pyautogui
+import numpy as np
 from async_tkinter_loop import async_handler, async_mainloop
 
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////
 # GLOBAL DEFINES
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////
-KELVIN = 5000    # 2000 to 8000, where 2000 is the warmest and 8000 is the coolest
-DECIMATE = 10   # skip every DECIMATE number of pixels to speed up calculation
-DURATION = 500  # The time over which to change the colour of the lights in ms. Use 100 for faster transitions
+KELVIN = 8000  # 2000 to 8000, where 2000 is the warmest and 8000 is the coolest
+DECIMATE = 10  # skip every DECIMATE number of pixels to speed up calculation
+DURATION = 1000  # The time over which to change the colour of the lights in ms. Use 100 for faster transitions
 BLACK_THRESHOLD = 0.2  # Black Screen Detection Threshold
 # Black Screen case's brightness setting
-BLACK_BRIGHTNESS = int(round(0x10000 * 0.03*360)/360) % 0x10000
+BLACK_BRIGHTNESS = int(round(0x10000 * 0.03 * 360) / 360) % 0x10000
 BLACK_KELVIN = 6000  # Black Screen case's Kelvin setting
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+MAX_RETRY_ATTEMPTS = 10
+RETRY_DELAY_SECONDS = 5
+
+MAX_DISCOVERY_RETRIES = 5
+DISCOVERY_RETRY_DELAY_SECONDS = 5
+
+
+# ... (rest of your constants and global variables)
+def find_average_colors(image):
+    red, green, blue = 0, 0, 0
+    for y in range(0, image.height, DECIMATE):
+        for x in range(0, image.width, DECIMATE):
+            color = image.getpixel((x, y))
+            if sum(color) == 0:
+                continue
+            red += color[0]
+            green += color[1]
+            blue += color[2]
+
+    # calculate the averages
+    total_pixels = (image.height // DECIMATE) * (image.width // DECIMATE)
+    red_average = red / total_pixels
+    green_average = green / total_pixels
+    blue_average = blue / total_pixels
+
+    # clamp values to [0, 1]
+    red_average = min(red_average / 255.0, 1.0)
+    green_average = min(green_average / 255.0, 1.0)
+    blue_average = min(blue_average / 255.0, 1.0)
+
+    # generate a composite colour from these averages
+    c = Color(rgb=(red_average, green_average, blue_average))
+
+
+async def find_vibrant_colors(image, bulb):
+    pixel_colors = {}
+
+    for y in range(0, image.height, DECIMATE):
+        for x in range(0, image.width, DECIMATE):
+            # Get the RGB color of the pixel
+            color = image.getpixel((x, y))
+            if sum(color) == 0:
+                continue
+            red = color[0]
+            green = color[1]
+            blue = color[2]
+
+            # Convert RGB to HSV (Hue, Saturation, Value) color space using the colour module
+            color = Color(rgb=(red / 255, green / 255, blue / 255))
+            if (color.get_luminance() > 0.8) or (color.get_luminance() < 0.15):
+                continue
+
+            # Store the pixel color and its saturation and brightness in the dictionary
+            pixel_colors[(red, green, blue)] = (
+                color.saturation,
+                color.luminance,
+                color.hue,
+            )
+
+    # Sort the pixel colors based on their occurrence frequency
+    sorted_colors = sorted(pixel_colors.items(), key=lambda item: item[1], reverse=True)
+
+    # Exclude colors that are too similar to each other
+    final_colors = []
+    for color, (saturation, luminance, hue) in sorted_colors:
+        exclude_color = False
+        for final_color in final_colors:
+            # Calculate the difference in saturation and luminance
+            saturation_diff = abs(final_color[1][0] - saturation)
+            luminance_diff = abs(final_color[1][1] - luminance)
+            if saturation_diff < 0.1 and luminance_diff < 0.1:
+                exclude_color = True
+                break
+        if not exclude_color:
+            final_colors.append((color, (saturation, luminance, hue)))
+
+        # Break the loop when we have found 4 colors
+        if len(final_colors) == 4:
+            break
+
+    hex_values = [
+        Color(rgb=(r / 255.0, g / 255.0, b / 255.0)).hex
+        for (r, g, b), _ in final_colors
+    ]
+    print(hex_values)
+    for c in final_colors:
+        # We create the widgets here
+
+        red, green, blue = c[0]
+
+        # Extract the saturation and luminance values from the second element of the tuple
+        saturation, luminance, hue = c[1]
+        setLight(red, green, blue, hue, saturation, luminance, bulb, False)
+        await asyncio.sleep(5)
+
+
+def setLight(red, green, blue, hue, saturation, luminance, bulb, fast):
+    hue = min(int(round(0x10000 * hue * 360) / 360) % 0x10000, 65535)
+    saturation = min(int(round(0xFFFF * saturation)), 65535)
+    brightness = min(int(round(0xFFFF * luminance)), 65535)
+
+    if (
+        (red < BLACK_THRESHOLD)
+        and (green < BLACK_THRESHOLD)
+        and (blue < BLACK_THRESHOLD)
+    ):
+        # print("black1 detected")
+        bulb.set_color([0, 0, BLACK_BRIGHTNESS, BLACK_KELVIN], DURATION, fast)
+
+    else:
+        bulb.set_color([hue, saturation, brightness, KELVIN], DURATION, fast)
+
+    win.config(bg="#%02x%02x%02x" % c[0])
+
+
+async def discover_lights():
+    num_retries = 0
+    while num_retries < MAX_DISCOVERY_RETRIES:
+        try:
+            lifx = LifxLAN()
+            # get devices
+            devices = lifx.get_lights()
+            # Filter out bulbs with no label (name)
+            devices_with_label = [device for device in devices if device.get_label()]
+            if devices_with_label:
+                return devices_with_label[
+                    0
+                ]  # Return the first discovered bulb with a label
+        except Exception as e:
+            print("Error during light discovery: {}. Retrying...".format(e))
+            num_retries += 1
+            await asyncio.sleep(DISCOVERY_RETRY_DELAY_SECONDS)
+
+    print("Max discovery retries reached or no lights with labels found. Exiting.")
+    return None
+
+
 async def screen():
-    num_lights = None
     if len(sys.argv) != 2:
-        print("\nDiscovery will go much faster if you provide the number of lights on your LAN:")
+        print(
+            "\nDiscovery will go much faster if you provide the number of lights on your LAN:"
+        )
         print("  python {} <number of lights on LAN>\n".format(sys.argv[0]))
     else:
         num_lights = int(sys.argv[1])
@@ -39,12 +176,22 @@ async def screen():
     # lifx = LifxLAN() works just as well. Knowing the number of bulbs in advance
     # simply makes initial bulb discovery faster.
     print("Discovering lights...")
-    lifx = LifxLAN(num_lights)
+    # Discover lights and initialize bulb
+    bulb = await discover_lights()
 
-    # get devices
-    devices = lifx.get_lights()
-    bulb = devices[0]
-    print("Selected {}".format(bulb.get_label()))
+    if not bulb:
+        print("No lights found. Exiting.")
+        return
+
+    # Check if the bulb has a label
+    try:
+        label_response = bulb.req_with_resp(GetLabel, StateLabel)
+        print("Selected bulb label: {}".format(label_response.label))
+    except Exception as e:
+        print("Error occurred while retrieving label: {}".format(e))
+        return
+
+    print("Selected bulb label: {}".format(bulb.get_label()))
 
     # get original state
     original_power = bulb.get_power()
@@ -53,75 +200,39 @@ async def screen():
 
     sleep(0.2)  # to look pretty
 
-    num = 1
+    # Main retry loop for screen processing
+    num_retries = 0
+    while num_retries < MAX_RETRY_ATTEMPTS:
+        try:
+            # init counters/accumulators
 
-    while num == 1:
-        # init counters/accumulators
-        red = 0
-        green = 0
-        blue = 0
+            # Just take the middle 15% of the screen
+            screen_width, screen_height = pyautogui.size()
 
-        # Crop a chunk of the screen out
-        # This is hacky and is currently screen and movie-size specific.
-        # To get these values, I take a screenshot and use Paint.Net to easily find the coordinates
-        # TODO: clean this up and make it dynamically detect size and crop the black bits out automagically
-        left = 0      # The x-offset of where your crop box starts
-        top = 140    # The y-offset of where your crop box starts
-        width = 1920   # The width  of crop box
-        height = 800    # The height of crop box
-        box = (left, top, left+width, top+height)
+            crop_height = int(screen_height * 0.15)
 
-        # take a screenshot
-        image = ImageGrab.grab(bbox=box)
-        # //////////////////////////////////////////////////////////////////////////////////////////////////////////
-        # Left Side of Screen
-        # //////////////////////////////////////////////////////////////////////////////////////////////////////////
-        for y in range(0, height, DECIMATE):  # loop over the height
-            for x in range(0, width, DECIMATE):
-                color = image.getpixel((x, y))  # grab a pixel
-                # calculate sum of each component (RGB)
-                if ((color[0]+color[1]+color[2]) == 0):
+            # Crop a chunk of the screen out
+            # This is hacky and is currently screen and movie-size specific.
+            # To get these values, I take a screenshot and use Paint.Net to easily find the coordinates
+            left = 0
+            top = (screen_height - crop_height) // 2
+            width = screen_width
+            height = crop_height
+            box = (left, top, left + width, top + height)
 
-                    next
-                red = red + color[0]
-                green = green + (color[1])
-                blue = blue + color[2]
+            # take a screenshot
+            # bbox=box
+            image = ImageGrab.grab()
 
-        # calculate the averages
-        red = (((red / ((height/DECIMATE) * (width/DECIMATE))))/255.0)
-        green = (((green / ((height/DECIMATE) * (width/DECIMATE))))/255.0)
-        blue = (((blue / ((height/DECIMATE) * (width/DECIMATE))))/255.0)
+            await find_vibrant_colors(image, bulb)
 
-        # generate a composite colour from these averages
-        c = Color(rgb=(red, green, blue))
-        # We create the widgets here
+        except Exception as e:
+            print("Error occurred: {}. Retrying...".e)
+            num_retries += 1
+            await asyncio.sleep(RETRY_DELAY_SECONDS)
 
-        print(c)
-        # print(original_color)
-
-        hue = min(
-            (int(round(0x10000 * c.hue*360)/360) % 0x10000), 65535)
-        saturation = min(int(round(0xFFFF * c.saturation)), 65535)
-        brightness = min(int(round(0xFFFF * c.luminance)), 65535)
-        # print(saturation)
-        print(c.hue*360, " ", c.saturation, " ", c.luminance)
-
-        # print "\naverage1  red:%s green:%s blue:%s" % (red,green,blue)
-        # print "average1   hue:%f saturation:%f luminance:%f" % (c.hue,c.saturation,c.luminance)
-        # print "average1  (hex) "+  (c.hex)
-        # //////////////////////////////////////////////////////////////////////////////////////////////////////////
-        # PROGRAM LIFX BULBS (LEFT)
-        # //////////////////////////////////////////////////////////////////////////////////////////////////////////
-        if (c.red < BLACK_THRESHOLD) and (c.green < BLACK_THRESHOLD) and (c.blue < BLACK_THRESHOLD):
-            # print("black1 detected")
-            bulb.set_color([0, 0, BLACK_BRIGHTNESS, BLACK_KELVIN], DURATION)
-
-        else:
-            bulb.set_color(
-                [hue, saturation, brightness, KELVIN], DURATION, True)
-
-        win.config(bg=c)
-        await asyncio.sleep(0.05)
+    if num_retries >= MAX_RETRY_ATTEMPTS:
+        print("Max retry attempts reached. Aborting.")
 
         # //////////////////////////////////////////////////////////////////////////////////////////////////////////
     # restore original color
@@ -136,10 +247,9 @@ async def screen():
 
 
 win = Tk()  # creating the main window and storing the window object in 'win'
-win.attributes('-topmost', True)
+win.attributes("-topmost", True)
 Button(win, text="Quit", command=win.destroy).pack()
 Button(win, text="Start", command=async_handler(screen)).pack()
 
-
-win.geometry('200x200')
+win.geometry("200x200")
 async_mainloop(win)
